87f2c630dd275ad50156bc96e5f4c336a6ca5844
Piyush Dhall <dhall.piyush@gmail.com>
2022-01-09 23:48:43 +0530

    string driver

--- 03-strings/09-string-input.cpp
+++ 03-strings/09-string-input.cpp
@@ -7,8 +7,14 @@ using namespace std;
 int main()
 {
     string sentence;
+    // can also put for loop in the string and it will print every character.
 
     getline(cin, sentence);
     cout << sentence << endl;
+
+    string hash;
+
+    getline(cin, hash, '#'); // hash is the delimiter
+    cout << hash << endl;
     return 0;
 }
---
+++ 03-strings/10-string-compression.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int compress(vector<char> &chars)
+{
+    // your code goes here
+}
+
+int main()
+{
+    vector<char> mystring = {'h', 'e', 'l', 'l', 'o', 'o', 'o', 'o'};
+    return 0;
+}
