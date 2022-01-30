aabc49b7a209bc78e3be25c23c7e88f79d540565
Piyush Dhall <dhall.piyush@gmail.com>
2022-01-30 18:00:12 +0530

    quick sort

--- 09-divide-and-conquer/02-quick-sort.cpp
+++ 09-divide-and-conquer/02-quick-sort.cpp
@@ -3,15 +3,44 @@ using namespace std;
 
 // helper function
 
-// sorting function
-void mergeSort(vector<int> &array)
+int partition(vector<int> &array, int s, int e)
 {
-    return;
+    int pivot = array[e];
+    int i = s - 1;
+
+    for (int j = s; j < e; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            swap(array[i], array[j]);
+        }
+    }
+    swap(array[i + 1], array[e]);
+    return i + 1;
+}
+
+// sorting function
+void quickSort(vector<int> &array, int s, int e)
+{
+    // base case
+    if (s >= e)
+    {
+        return;
+    }
+
+    // recursive case
+
+    int p = partition(array, s, e);
+    quickSort(array, s, p - 1);
+    quickSort(array, p + 1, e);
 }
 
 int main()
 {
     vector<int> array = {10, 5, 2, 6, 5, 4, 5, 7, 8, 1};
+    int n = array.size();
+    quickSort(array, 0, n - 1);
 
     for (int x : array)
     {
